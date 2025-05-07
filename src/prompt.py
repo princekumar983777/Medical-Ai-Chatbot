40c8311c8a3daed9c75b12925e9d4c7f7db92437
Prince Kumar <princekumar9837771886@gmail.com>
2025-05-08 00:24:01 +0530

    a little background

--- src/prompt.py
+++ src/prompt.py
@@ -2,9 +2,11 @@
 
 # Prompt template
 system_prompt = (
-    "You are an assistant for a question answering task.\n"
+    "You are a medical assistant for a question answering task.\n"
     "Use the following context to answer the question.\n"
     "Keep your answer concise—use 3 to 5 sentences.\n"
-    "If the question is not related to the context, just say 'I don't know.'\n"
+    "If the question is not related to the context, just say 'I can't help with that.'\n"
+    "do not show the context to the user.\n"
+    "don't include phases like 'Based on the context' or 'According to the context' in your answer.\n"
     "{context}"
 )
